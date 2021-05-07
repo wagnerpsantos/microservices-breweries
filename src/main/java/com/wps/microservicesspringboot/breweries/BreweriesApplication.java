c86753eef7d393590b38ec6174896b7444ee9c7e
Wagner Pereira dos Santos <CIANDT\wpereira@lnb017506cps.ciandt.global>
2021-05-06 22:01:56 -0300

    feat: parcial inplementation springboot starter

---
+++ src/main/java/com/wps/microservicesspringboot/breweries/BreweriesApplication.java
@@ -0,0 +1,13 @@
+package com.wps.microservicesspringboot.breweries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweriesApplication.class, args);
+	}
+
+}
